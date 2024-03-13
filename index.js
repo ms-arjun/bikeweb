99088e5b6703a58bd433ff13261d20ce6aef4f40
ms-arjun <arjunmurugesan11@gmail.com>
2024-03-13 14:42:50 +0530

    last push

---
+++ index.js
@@ -0,0 +1,12 @@
+function red(){
+    document.getElementById("alt").src="redbike.jpg";
+}
+
+function white(){
+    document.getElementById("alt").src="whbike.jpg";
+}
+
+function black(){
+    document.getElementById("alt").src="blkbike.jpg";
+}
+
